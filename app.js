a0bf110112c37e9c1a27a7d441a056a723e41124
Jose Antonio Gil <josegilmon@gmail.com>
2015-07-09 22:42:11 +0200

    CSS's para diseño adaptable

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var partials = require('express-partials');
+
 var routes = require('./routes/index');
 
 var app = express();
@@ -12,6 +14,8 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+app.use(partials());
 
 // uncomment after placing your favicon in /public
 app.use(favicon(__dirname + '/public/favicon.ico'));
