d90864358e761ff800db13efc02f6caebaa78cff
Jose Antonio Gil <josegilmon@gmail.com>
2015-07-18 17:50:15 +0200

    Despliegue DB en Heroku

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -4,7 +4,7 @@ var models = require("../models/models.js");
 // GET /quizes/question
 exports.question = function (req, res) {
 	//res.render('quizes/question', { pregunta: "Capital de Italia" });
-	models.Quiz.findAll().success(function (quiz) {
+	models.Quiz.findAll().then(function (quiz) {
 		res.render('quizes/question', { pregunta: quiz[0].pregunta });
 	})
 };
@@ -12,7 +12,7 @@ exports.question = function (req, res) {
 // GET /quizes/answer
 exports.answer = function (req, res) {
 	//var resultado = (req.query && req.query.respuesta && req.query.respuesta.toUpperCase() === "ROMA") ? "Correcto": "Incorrecto";
-	models.Quiz.findAll().success(function (quiz) {
+	models.Quiz.findAll().then(function (quiz) {
 		var resultado = req.query && req.query.respuesta && req.query.respuesta.toUpperCase() === quiz[0].respuesta.toUpperCase()
 			? "Correcto"
 			: "Incorrecto"
--- models/models.js
+++ models/models.js
@@ -1,21 +1,38 @@
 var path = require("path");
+
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name  = (url[6] || null);
+var user     = (url[2] || null);
+var pwd      = (url[3] || null);
+var protocol = (url[1] || null);
+var dialect  = (url[1] || null);
+var port     = (url[5] || null);
+var host     = (url[4] || null);
+var storage  = process.env.DATABASE_STORAGE;
+
 var Sequelize = require("sequelize");
 
-var sequelize = new Sequelize(null, null, null, {
-    dialect: "sqlite",
-    storage: "quiz.sqlite"
-})
+var sequelize = new Sequelize(DB_name, user, pwd,
+    {
+        dialect:  protocol,
+        protocol: protocol,
+        port:     port,
+        host:     host,
+        storage:  storage,
+        omitNull: true
+    }
+);
 
 var Quiz = sequelize.import( path.join( __dirname, "quiz") );
 
-sequelize.sync().success(function () {
-    Quiz.count().success(function (count) {
+sequelize.sync().then(function () {
+    Quiz.count().then(function (count) {
         if (count === 0) {
             Quiz.create({
                 pregunta: "¿Capital de Italia?",
                 respuesta: "Roma"
             })
-            .success(function () {
+            .then(function () {
                 console.log("Base de datos inicializada");
             })
         }
