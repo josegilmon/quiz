ed21e3d08d33c99435545e1e10ce8ec2391763ea
Jose Antonio Gil <josegilmon@gmail.com>
2015-07-19 00:11:52 +0200

    Creación de preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -39,3 +39,20 @@ exports.answer = function (req, res) {
 		;
 	res.render('quizes/answer', { quiz: req.quiz, respuesta: resultado });
 };
+
+// GET /quizes/new
+exports.new = function (req, res) {
+	var quiz = models.Quiz.build({
+		pregunta: "Pregunta",
+		respuesta: "Respuesta"
+	});
+	res.render('quizes/new', { quiz: quiz });
+};
+
+// POST /quizes/create
+exports.create = function (req, res) {
+	var quiz = models.Quiz.build( req.body.quiz );
+	quiz.save({fields: ["pregunta", "respuesta"]}).then(function () {
+		res.redirect("/quizes");
+	});
+};
--- routes/index.js
+++ routes/index.js
@@ -15,6 +15,8 @@ router.param('quizId', quizController.load);
 router.get('/quizes', 						quizController.index);
 router.get('/quizes/:quizId(\\d+)', 		quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer',	quizController.answer);
+router.get('/quizes/new',					quizController.new);
+router.post('/quizes/create',				quizController.create);
 
 /* GET authors page. */
 router.get('/author', authorController.author);
